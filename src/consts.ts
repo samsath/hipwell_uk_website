08be1a56b75941bd94d0048ca6eb811f78372b0c
Sam Hipwell <samhipwell@gmail.com>
2025-06-09 23:51:00 +0100

    feat: top component

--- src/consts.ts
+++ src/consts.ts
@@ -4,6 +4,6 @@
 export const SITE_TITLE = 'Sam Hipwell';
 export const SITE_DESCRIPTION = 'I help ambitious teams and founders architect, build, and scale modern software systems.';
 
-export const PRISMIC_REPO_NAME: string = import.meta.env.PUBLIC_PRISMIC_REPO_NAME || '';
-export const API_ENDPOINT: string = import.meta.env.PRISMIC_API_ENDPOINT || '';
-export const PRISMIC_ACCESS_TOKEN: string = import.meta.env.PUBLIC_PRISMIC_ACCESS_TOKEN || '';
+export const PRISMIC_REPO_NAME: string = import.meta.env.PUBLIC_PRISMIC_REPO_NAME || 'hipwell-uk';
+export const API_ENDPOINT: string = import.meta.env.PRISMIC_API_ENDPOINT || 'https://hipwell-uk.cdn.prismic.io/api/v2';
+export const PRISMIC_ACCESS_TOKEN: string = import.meta.env.PUBLIC_PRISMIC_ACCESS_TOKEN || 'MC5hRWRYR0JFQUFDQUFjeVhV.77-9R--_ve-_ve-_vSLvv71F77-977-9Ge-_ve-_vXbvv73vv73vv71u77-9He-_ve-_ve-_vXdObu-_vQfvv71Ie--_vQ';
